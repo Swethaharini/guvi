2ccd59fc909a2278253cd89af69f1954b5132332
Swethaharini <51352574+Swethaharini@users.noreply.github.com>
2019-07-03 11:53:42 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -3,11 +3,8 @@ str = ""
 for i in s: 
 	str = i + str
 return str
-
 s = input()
-
 print ("The original string is : ",end="") 
 print (s) 
-
 print ("The reversed string(using loops) is : ",end="") 
 print (reverse(s)) 
