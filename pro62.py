390895ba5b2e0651f2fff61f7c44b49090c9ee96
Swethaharini <51352574+Swethaharini@users.noreply.github.com>
2019-07-14 21:41:51 +0530

    Create pro62.py

---
+++ pro62.py
@@ -0,0 +1,10 @@
+a=input()
+m=0
+n=[]
+for i in range(0,len(a)-1):
+    for j in range(i+1,len(a)):
+        m=st1[i:j+1]
+        l=m[::-1]
+        if m==l:
+            c.append(len(a)-len(l))
+print(min(n))
