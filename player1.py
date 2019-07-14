2981b8d403e12b8173118d3b401b3a8d75200592
Swethaharni M <51352574+Swethaharini@users.noreply.github.com>
2019-07-14 22:28:49 +0530

    Update player1.py

--- player1.py
+++ player1.py
@@ -3,8 +3,8 @@ b=[]
 j = list(map(int,input().split()))
 c=[]
 for i in j:
-    if(l.count(j)>1):
-        c.append(j)
+    if(j.count(i)>1):
+        c.append(i)
 if (len(c)>=2):
     d=set(c)
     print(*d)
