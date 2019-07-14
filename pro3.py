07bd465d828bfd058cb2a4be3fae0c38585c0bc7
Swethaharini <51352574+Swethaharini@users.noreply.github.com>
2019-07-14 21:36:30 +0530

    Update pro3.py

--- pro3.py
+++ pro3.py
@@ -1,8 +1,8 @@
 a,b=input().split()
 r=abs(len(b)-len(a))
-for k in range(len(a)):
-  if(len(b)==1 and p[k] in a):
+for l in range(len(a)):
+  if(len(b)==1 and p[l] in a):
     break
-  if(a[k]!=b[k]):
+  if(a[l]!=b[l]):
     r=r+1
 print(r)
