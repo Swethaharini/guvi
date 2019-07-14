dacddc6d6baacb49160959d88231c6444f484524
Swethaharini <51352574+Swethaharini@users.noreply.github.com>
2019-07-14 21:46:03 +0530

    Update pro62.py

--- pro62.py
+++ pro62.py
@@ -3,7 +3,7 @@ m=0
 n=[]
 for i in range(0,len(a)-1):
     for j in range(i+1,len(a)):
-        m=st1[i:j+1]
+        m=a[i:j+1]
         l=m[::-1]
         if m==l:
             n.append(len(a)-len(l))
